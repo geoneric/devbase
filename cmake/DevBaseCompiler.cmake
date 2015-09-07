e57521c9ba6aca0cc737762505edb5361bb4e3de
Kor de Jong <k.dejong@geoneric.eu>
2015-09-07 09:40:14 +0200

    Reordered warning options

--- cmake/DevBaseCompiler.cmake
+++ cmake/DevBaseCompiler.cmake
@@ -63,7 +63,7 @@ if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" OR
     # -Wconversion
     # -Wsign-conversion
     set(CMAKE_CXX_FLAGS
-        "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wcast-qual -Wwrite-strings -Werror=strict-aliasing -pedantic -fno-strict-overflow -ftemplate-backtrace-limit=0"
+        "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -Wcast-qual -Wwrite-strings -Werror=strict-aliasing -fno-strict-overflow -ftemplate-backtrace-limit=0"
     )
 
     # This results in an error on mingw/gcc 4.8/windows. Some warning about
