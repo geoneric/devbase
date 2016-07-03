eef28d1ab3266557564d148317d85e537a4d0ea0
Kor de Jong <k.dejong@geoneric.eu>
2016-07-03 19:17:03 +0200

    Moved peacock config DevBaseExternal -> DevBaseCompiler

--- cmake/DevBaseCompiler.cmake
+++ cmake/DevBaseCompiler.cmake
@@ -132,3 +132,35 @@ endif()
 if(WIN32)
     set(CMAKE_DEBUG_POSTFIX "d")
 endif()
+
+
+# Peacock is a project for building external software. It can be used to
+# build the Boost libraries on all kinds of platform, for example. A
+# PEACOCK_PREFIX CMake variable or environment variable can be set to
+# point us to the root of the platform-specific files. By adding the
+# current platform string to this prefix, we end up at the root of the
+# header files and libraries.
+# See also: https://github.com/geoneric/peacock
+if(peacock_compiler_found)
+    # If the PEACOCK_PREFIX CMake variable is not set, but an environment
+    # variable with that name is, then copy it to a CMake variable. This way
+    # the CMake variable takes precedence.
+    if((NOT PEACOCK_PREFIX) AND (DEFINED ENV{PEACOCK_PREFIX}))
+        set(PEACOCK_PREFIX $ENV{PEACOCK_PREFIX})
+    endif()
+
+    if(PEACOCK_PREFIX)
+        # PEACOCK_PREFIX takes precedence over all other paths.
+
+        # # if cross compiling:
+        # set(CMAKE_FIND_ROOT_PATH
+        #     ${PEACOCK_PREFIX}/${peacock_target_platform})
+        # else:
+        set(CMAKE_PREFIX_PATH
+            ${PEACOCK_PREFIX}/${peacock_target_platform}
+            ${CMAKE_PREFIX_PATH}
+        )
+        message(STATUS "Probing Peacock builds in: ${PEACOCK_PREFIX}/${peacock_target_platform}")
+        set(CMAKE_INCLUDE_DIRECTORIES_BEFORE TRUE)
+    endif()
+endif()
