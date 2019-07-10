dfd728c3982e271de1307b8f38f9b3a16397f263
Kor de Jong <k.dejong@geoneric.eu>
2019-07-10 10:55:52 +0200

    Add support for separating UTF arguments from runtime arguments

--- cmake/DevBaseMacro.cmake
+++ cmake/DevBaseMacro.cmake
@@ -168,13 +168,22 @@ endmacro()
 #
 # SCOPE: Some prefix. Often the lib name of the lib being tested
 # NAME : Name of test module, without extension
+# UTF_ARGUMENTS_SEPARATOR: String to put between the command and the
+#     UTF arguments.
+#     TODO: This is how it could work:
+#     <command> <runtime_arguments>
+#         <utf_arguments_separator> <utf_arguments>
+#         <command_arguments_separator> <command_arguments>
+#     HPX: utf_arguments_separator == '--'
+#     UTF: command_arguments_separator == '--'
+#         (so this separator doesn't have to be passed in!)
 # LINK_LIBRARIES: Libraries to link against
 # DEPENDENCIES: Targets this test target depends on
 # ENVIRONMENT: Environment variables that should be defined for running
 #     the test
 macro(add_unit_test)
     set(OPTIONS "")
-    set(ONE_VALUE_ARGUMENTS SCOPE NAME)
+    set(ONE_VALUE_ARGUMENTS SCOPE NAME UTF_ARGUMENTS_SEPARATOR)
     set(MULTI_VALUE_ARGUMENTS
         SUPPORT_NAMES
         INCLUDE_DIRS
@@ -220,7 +229,9 @@ macro(add_unit_test)
         # catch_system_errors: Prevent UTF to detect system errors. This
         #     messes things up when doing system calls to Python unit tests.
         #     See also: http://lists.boost.org/boost-users/2009/12/55048.php
-        COMMAND ${TEST_EXE_NAME} --catch_system_errors=no)
+        COMMAND ${TEST_EXE_NAME} ${ADD_UNIT_TEST_UTF_ARGUMENTS_SEPARATOR}
+            --catch_system_errors=no
+    )
 
     if(ADD_UNIT_TEST_DEPENDENCIES)
         ADD_DEPENDENCIES(${TEST_EXE_NAME} ${ADD_UNIT_TEST_DEPENDENCIES})
@@ -248,7 +259,7 @@ endmacro()
 
 function(add_unit_tests)
     set(OPTIONS "")
-    set(ONE_VALUE_ARGUMENTS SCOPE)
+    set(ONE_VALUE_ARGUMENTS SCOPE UTF_ARGUMENTS_SEPARATOR)
     set(MULTI_VALUE_ARGUMENTS
         NAMES
         SUPPORT_NAMES
@@ -273,6 +284,7 @@ function(add_unit_tests)
         add_unit_test(
             SCOPE ${ADD_UNIT_TESTS_SCOPE}
             NAME ${NAME}
+            UTF_ARGUMENTS_SEPARATOR ${ADD_UNIT_TESTS_UTF_ARGUMENTS_SEPARATOR}
             SUPPORT_NAMES ${ADD_UNIT_TESTS_SUPPORT_NAMES}
             INCLUDE_DIRS ${ADD_UNIT_TESTS_INCLUDE_DIRS}
             OBJECT_LIBRARIES ${ADD_UNIT_TESTS_OBJECT_LIBRARIES}
